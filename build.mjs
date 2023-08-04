8e60892e9248b6f0a12f0577b7a0a5e3f2465294
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-08-03 19:31:02 -0600

    release-0.1.3

--- build.mjs
+++ build.mjs
@@ -1,9 +1,13 @@
 import dts from 'bun-plugin-dts'
+import pkg from './package.json'
 
-await Bun.build({
+const build = await Bun.build({
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
   minify: true,
   splitting: true,
   plugins: [dts()]
 })
+
+
+
